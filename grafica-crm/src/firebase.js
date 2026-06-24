dd4ae8dd7223637e1b4b6baa34cb33e102c973b6
contatoalfaprint7-create <contatoalfaprint7@gmail.com>
2026-06-24 18:45:09 -0300

    Update firebase.js

--- grafica-crm/src/firebase.js
+++ grafica-crm/src/firebase.js
@@ -1,20 +1,14 @@
-﻿// src/firebase.js
-// apiKey: "AIzaSyAHLaLbKvXXA1HOsjgcDpCghjUq137XV7Y",
+﻿import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHLaLbKvXXA1HOsjgcDpCghjUq137XV7Y",
   authDomain: "grafica-crm.firebaseapp.com",
   databaseURL: "https://grafica-crm-default-rtdb.firebaseio.com",
   projectId: "grafica-crm",
   storageBucket: "grafica-crm.firebasestorage.app",
   messagingSenderId: "210284881910",
-  appId: "1:210284881910:web:e87db6e98a56a68aea89d7"} from "firebase/database";
-
-const firebaseConfig = {
-  apiKey:            "COLE_AQUI_SUA_API_KEY",
-  authDomain:        "COLE_AQUI.firebaseapp.com",
-  databaseURL:       "https://COLE_AQUI-default-rtdb.firebaseio.com",
-  projectId:         "COLE_AQUI",
-  storageBucket:     "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId:             "COLE_AQUI"
+  appId: "1:210284881910:web:e87db6e98a56a68aea89d7"
 };
 
 const app = initializeApp(firebaseConfig);
